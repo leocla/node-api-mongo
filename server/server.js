239a920b79a26a07eb5deacb082b45a0529fce82
Rahmat O <rahmat.oktrifianto@mail.ugm.ac.id>
2018-06-26 00:45:53 +0700

    edit port

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ var env = process.env.NODE_ENV || 'development';
 console.log('env ******', env);
 
 if(env === 'development'){
-    process.env.PORT = 4000;
+    //process.env.PORT = 4000;
     //process.env.MONGODB_URI = ('mongodb://localhost:27017/TodoAplikasi') || ('mongodb://tono:tono1234@ds117431.mlab.com:17431/node-api-mongodb');
     process.env.MONGODB_URI = 'mongodb://tono:tono1234@ds117431.mlab.com:17431/node-api-mongodb';
 } else if(env === 'test'){
